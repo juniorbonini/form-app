47ba6022d00ec65a9c98dcfc76a1bcc52b716465
juniorbonini <juniorboninia7x@gmail.com>
2025-11-05 09:50:16 -0300

    Estrutura inicial do projeto. - Home - screen / Input - component

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./style";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Criar sua conta</Text>
+    </View>
+  );
+}
---
+++ src/screens/Home/style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#F4F3F5',
+        padding: 24
+    },
+     title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+     }
+})
