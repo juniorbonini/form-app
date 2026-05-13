a2ee3346ba88d73fe9a69c3eb9a803588b94e48a
juniorbonini <juniorboninia7x@gmail.com>
2026-05-13 19:46:48 -0300

    feat(js): funções auxiliares de DOM getElement e setValue

--- src/public/script/index.js
+++ src/public/script/index.js
@@ -1,2 +1,7 @@
 const SESSION_KEY = "cadastrolab_form";
 const CEP_API = "https://viacep.com.br/ws/";
+
+const getElement = (id) => document.getElementById(id);
+const getValue = (id) => getElement(id).value;
+const setValue = (id, value) => (getElement(id).value = value);
+const clearValue = (id) => (getElement(id).value = "");
